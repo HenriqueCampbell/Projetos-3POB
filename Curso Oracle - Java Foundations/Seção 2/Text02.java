94a6bcfac9713fe270298c607f4af4f5f92bd0f0
Henrique Campbell <hctrechau@gmail.com>
2026-03-04 10:59:03 -0300

    Create Text02.java

---
+++ Curso Oracle - Java Foundations/Seção 2/Text02.java
@@ -0,0 +1,10 @@
+public class Text02 {
+    public static void meow() {
+        System.out.println("Meow!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("\n\n\n\n");
+        meow();
+    }
+}
